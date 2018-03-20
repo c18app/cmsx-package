c045e158a38d635d0b5681126b6fac7a6b5cfc73
Michal Nosavcov <mnosavcov@gmail.com>
2018-03-20 18:29:05 +0100

    úprava editace stránek

--- src/Controllers/CmsController.php
+++ src/Controllers/CmsController.php
@@ -16,12 +16,28 @@ class CmsController extends Controller
     public function page($id)
     {
         $page = Page::findOrFail($id);
+        $page->formatedContent = $this->prepareContent($page->content);
         return view('cmsx::cms.page', ['page' => $page]);
     }
 
     public function article($id)
     {
         $article = Article::findOrFail($id);
+        $article->formatedContent = $this->prepareContent($article->content);
+
         return view('cmsx::cms.article', ['article' => $article]);
+    }
+
+    private function prepareContent($content) {
+        $temp = preg_split("/\R\R/", $content);
+
+        $result = [];
+        foreach($temp as $item) {
+            $result[] = '<p>'.nl2br(trim($item)).'</p>';
+        }
+
+        $result = implode("\n", $result);
+
+        return $result;
     }
 }
